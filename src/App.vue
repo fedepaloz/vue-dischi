89f298d96bee54844bae2cae64ca3029eb12ba52
fedepaloz <palozzifederico@gmail.com>
2022-07-27 15:53:26 +0200

    sezione tracce

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
     <TheHeader></TheHeader>
+    <TrackSection></TrackSection>
   </div>
 </template>
 
-<script>
-import TheHeader from './components/TheHeader.vue';
-export default{
-  name:'App',
-  components:{
-    TheHeader,
-    
-}
-}
+<script> 
+
+import TheHeader from "./components/TheHeader.vue";
+import TrackSection from "./components/TrackSection.vue";
+export default {
+  name: "App",
+  components: {
+    TheHeader : TheHeader,
+    TrackSection : TrackSection,
+  },
+};
 </script>
 
 <style lang="scss">
-@import './assets/scss/style.scss';
+@import "./assets/scss/style.scss";
 </style>
---
+++ src/components/TrackSection.vue
@@ -0,0 +1,36 @@
+<template>
+  <section id="TrackSection">
+    <div class="track-list container">
+        <div class="row">
+            <div class="col-3 text-white" v-for="(track,index) in tracks" :key="index">{{track.title}}</div>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "TrackSection",
+  data() {
+    return {
+      tracks: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        this.tracks = res.data;
+      });
+  },
+};
+</script>
+
+
+<style scoped>
+#TrackSection {
+  background-color: #1e2d3b;
+  height: 100vh;
+}
+</style>
